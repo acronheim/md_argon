2d80d21818d0491782bcbf16b51b5b6753b87ab2
Alexander Cronheim <alexander.cronheim@gmail.com>
2015-02-17 16:52:28 +0100

    overal code cleanup and correction in implementation of the  maxwell botzman velocity  distribution

--- argon-box.f90
+++ argon-box.f90
@@ -10,59 +10,47 @@ program argon_box
 	!number of fcc cells per cartesian dimension = Ncell, 
 	!number of particles is N, (4 particles per cube)		
 	integer, parameter :: N_cell_dim = 6, N_cell = N_cell_dim**3, N_part = N_cell*4
-	real(8), parameter :: L_side = 1d0, T = 1, m = 1, s = 1, dt = 1d-2, t_stop = 1
+	real(8), parameter :: L_side = 1d0, T = 1, m = 1, s = 1, dt = 1d-9, t_stop = 1
 	real(8), parameter :: PI = 4 * atan(1d0), Kb = 1 	!constants	
 	integer :: i,j,k,l,n !iteration variables
-	real(8) :: xs(2) !random number variable
+	real(8):: xs(2) !two random numbers
 	real(8) :: time, r, F(3) 
 
-	! face centered cubic unit cell with basis particle positions:
-	real(8), dimension(1:3), parameter :: &
-		fcc_part1 = (/0d0,  0d0,  0d0/),  &
-		fcc_part2 = (/0d0,  5d-1, 5d-1/), &
-		fcc_part3 = (/5d-1, 0d0,  5d-1/), &
-		fcc_part4 = (/5d-1, 5d-1, 0d0/)		
-	real(8), dimension(1:3,1:4), parameter :: &
-	R_cell = reshape( (/fcc_part1, fcc_part2, fcc_part3, fcc_part4/), (/3,4/))
+
 	
 	! particle system arrays
 	real(8), dimension(1:3, 1:N_part) :: pos, vel
 	
-
-!	print *, "face centered cubic unit cell"
-!	do i = 1,3
-!		print *, (R_cell(i,j), j=1,4)
-!	end do
 	
 	!initial positions of all particles according to an fcc lattice structure
 	print *, "initializing initial particle positions"
-	
 	n = 0
 	do i = 1,N_cell_dim
 		do j = 1,N_cell_dim
 			do k = 1,N_cell_dim
 				do l = 1,4
 					n = n + 1
-					pos(:,n) = L_side/N_cell_dim*( (/ i-1, j-1, k-1 /) + R_cell(:,l) )
+					pos(:,n) = L_side/N_cell_dim*((/ i-1, j-1, k-1 /) + fcc_cell(l))
 					print *, "particle:", n, "/",  N_part, "position:", pos(:,n)
 				end do
 			end do
 		end do
 	end do
 	
+	
 	!initial particles velocities according to the maxwell distribution
 	print *, "initializing initial particle velocities"
 	call init_random_seed
 	do n = 1,N_part
 		do i = 1,3
-			! each velocity component is normally distributed:
-			! f(v) = sqrt(m/(2*PI*Kb*T))*exp(-m/(2*Kb*T) * (v**2))
+			! in the maxwell boltzman distribution each velocity component is normally distributed:
+			! f(v) = sqrt(m/(2*PI*Kb*T)) * exp(-(v**2)/2 *m/(*Kb*T))
+			! sigma**2 = Kb*T/m, and zero mean
 			! -> box-muller transform from uniform to normal distribution
 			CALL RANDOM_NUMBER(xs(1))
-			CALL RANDOM_NUMBER(xs(2))
-			
+			CALL RANDOM_NUMBER(xs(2))						
 			! how to incorporate the standard deviation?
-			vel(i,n) = sqrt(m/(2*PI*Kb*T)) * box_muller(xs)
+			vel(i,n) = sqrt(Kb*T/m) * normal_rnd(xs)
 		end do
 		print *,"particle:", n, "/",  N_part, "velocity:", VEL(:,n)
 	end do
@@ -121,15 +109,44 @@ call plot_end
 
 contains
 	
+	function fcc_cell(i) result(output)
+		implicit none
+		integer, intent(in) :: i
+		real(8) :: output(3)
+		! face centered cubic unit cell with basis particle positions:
+		real(8), dimension(1:3), parameter :: &
+			fcc_part1 = (/0d0,  0d0,  0d0/),  &
+			fcc_part2 = (/0d0,  5d-1, 5d-1/), &
+			fcc_part3 = (/5d-1, 0d0,  5d-1/), &
+			fcc_part4 = (/5d-1, 5d-1, 0d0/)		
+		real(8), dimension(1:3,1:4), parameter :: &
+		R_cell = reshape( (/fcc_part1, fcc_part2, fcc_part3, fcc_part4/), (/3,4/))
+		output = R_cell(:,i)
+		
+	!	print *, "face centered cubic unit cell"
+	!	do i = 1,3		!		print *, (R_cell(i,j), j=1,4)
+	!	end do
+		
+	end function fcc_cell
+	
 	! adapted from ICCP coding-notes
 	function box_muller(xs) result(zs)
 	  implicit none
 	  real(8) :: pi = 4*atan(1d0), zs !(2)
 	  real(8), intent(in) :: xs(2)
-
+	  
 	  zs = sqrt(-2d0*log(xs(1)))*cos(2*pi*xs(2))
 	  !zs(2) = sqrt(-2d0*log(xs(1)))*sin(2*pi*xs(2))
 	end function box_muller
+	
+	function normal_rnd(xs) result(norm_rnd_variable)
+		!normal distribution
+		implicit none
+		real(8), intent(in) :: xs(2) 
+		real(8) :: norm_rnd_variable
+		
+		norm_rnd_variable = box_muller(xs)		
+	end function normal_rnd
 	
 	! copied from ICCP coding-notes
 	subroutine init_random_seed()
