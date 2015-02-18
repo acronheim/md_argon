73f05eae52170872aa89f6b64e80cbb6a3ad7041
Alexander Cronheim <alexander.cronheim@gmail.com>
2015-02-18 02:08:30 +0100

    Energy, Temperature and Pressure measurement implemented, the latter not yet working..

--- argon-box.f90
+++ argon-box.f90
@@ -10,36 +10,36 @@ program argon_box
 	use plplot
     implicit none
 	
-	integer, parameter :: N_cell_dim = 3, N_cell = N_cell_dim**3, N_part = N_cell*4
-	real(8), parameter :: L_side = 1d0, T = 1000000000, m = 1, s = 1, dt = 1d-7, t_stop = 1
+	integer, parameter :: N_cell_dim = 2, N_cell = N_cell_dim**3, N_part = N_cell*4
+	real(8), parameter :: L_side = 1d0, T = 9000000, m = 1, s = 1, e = 1
+	real(8), parameter :: r_cut = L_side, dt = 1d-6, t_stop = 1
 	real(8), parameter :: PI = 4 * atan(1d0), Kb = 1 	!constants	
-	integer :: i,j,k,l,n !iteration variables
+	integer, parameter :: N_avSteps = 10 ! #steps used for ensemble average
+	integer :: i,j,k,l,n, step!iteration variables
 	real(8):: xs(2) !two random numbers
-	real(8) :: time, r, r_vec(3), F(3) 
+	real(8) :: time, r, r_vec(3), F(3), U(N_part), v_2(N_part), H, Ekin, P, F_R(N_avSteps), Temp
 	real(8), dimension(1:3, 1:N_part) :: pos, vel 	! particle system arrays	
+	
 	
 	!initial positions of all particles according to an fcc lattice structure
 	print *, "initializing initial particle positions"
 	n = 0
 	do i = 1,N_cell_dim
-		do j = 1,N_cell_dim
-			do k = 1,N_cell_dim
-				do l = 1,4
-					n = n + 1
-					pos(:,n) = L_side/N_cell_dim*((/ i-1, j-1, k-1 /) + fcc_cell(l))
-!					print *, "particle:", n, "/",  N_part, "position:", pos(:,n)
-				end do
-			end do
+	do j = 1,N_cell_dim
+	do k = 1,N_cell_dim
+		do l = 1,4
+			n = n + 1
+			pos(:,n) = L_side/N_cell_dim*((/ i-1, j-1, k-1 /) + fcc_cell(l))
+!			print *, "particle:", n, "/",  N_part, "position:", pos(:,n)
 		end do
 	end do
-	
-	
-	!initial particles velocities according to the maxwell distribution
-	
+	end do
+	end do
+		
+	!initial particles velocities according to the maxwell distribution	
 	! in the maxwell boltzman distribution each velocity component is normally distributed:
 	! f(v) = sqrt(m/(2*PI*Kb*T)) * exp(-(v**2)/2 *m/(*Kb*T))
-	! sigma**2 = Kb*T/m, and zero mean
-	
+	! sigma**2 = Kb*T/m, and zero mean	
 	print *, "initializing initial particle velocities"
 	call init_random_seed
 	do n = 1,N_part
@@ -50,56 +50,71 @@ program argon_box
 		end do
 !		print *,"particle:", n, "/",  N_part, "velocity:", VEL(:,n)
 	end do
-	
-	
-print *, "calculating time evolution"
 
 ! linear aproximation: x = x + v*dt, dv= F/m*dt, read verlets paper to find out whether higher order terms are needed.
 ! also the order in whidh dx and dv applied to the system might be significant.
-
-!time evolution for particles in lennard jones potential: U = 4(s/r)**12-(s/r)**6,
-!Fij = -du/dx = -du/dr*dr/dx = (48*s**12/r**13 - 6*s**6/r**7) * x/r, 
+!time evolution for particles in lennard jones potential: U = 4*e*(s/r)**12-(s/r)**6,
+!Fij = -du/dx = -du/dr*dr/dx = e*(48*s**12/r**13 - 6*s**6/r**7) * x/r, 
 !r = sqrt(x**2+y**2+z**2)	
-	
+print *, "calculating time evolution"
 time = 0d0
+step = 0
 call plot_init(0d0, L_side,0d0, L_side,0d0, L_side) 
 
+!Time integration
 do while (time < t_stop)
-	time = time + dt
-	call plot_points(pos)	
-	do n = 1,N_part			
-		pos(:,n) = pos(:,n) + vel(:,n)*dt			
-		do i = 1,3 !implements periodic boundary conditions
-			if (pos(i,n) < 0d0) then 
-				pos(i,n) = pos(i,n) + L_side !floor(pos(i,n)/L_side)*L_side
-				print *, n, "particle crossed boundary at zero, in dimension", i
-			else if (pos(i,n) > L_side) then
-				pos(i,n) = pos(i,n) - L_side !floor((pos(i,n)-L_side)/L_side)*L_side
-				print *, n, "particle crossed boundary at L, in dimension", i
-			end if
-		end do		
-!		print *, "particle:", n, "/",  N_part, "position:", pos(:,n)
-	end do	
+	call plot_points(pos)
+	time = time + dt	
+	step = step + 1	
 	
-	print *, "calculating dv for t = ", time, "+ dt"
-	do n = 1,N_part	
+	F_R(1:(N_avSteps-1)) = F_R(2:N_avSteps) !calculation of ensamble average as time average in virial theorem, pressure calculation
+	
+		! Force calculation	
+		do n = 1,N_part 
+		U(n) = 0 !potential
 		F = 0
 		do i = 	1,N_part !integrate over all particles inside box except i = n					
 			do j = -1, 1 
 			do k = -1, 1 !periodic boundary condition for potential forces..
 			do l = -1, 1				
-				if (n/=i) then !(.not.(n=i .and. (/j,k,l/)=(/0,0,0/)))	
+				if (n/=i)then !(.not.(n=i .and. (/j,k,l/)=(/0,0,0/)))	
 					r_vec = (/pos(1,n)-pos(1,i), pos(2,n)-pos(2,i), pos(3,n)-pos(3,i)/) + L_side*(/j,k,l/)
-					r = sqrt(DOT_PRODUCT(r_vec, r_vec))  
-					F = F + (48*s**12/r**14 - 6*s**6/r**8) * r_vec
+					r = sqrt(dot_product(r_vec, r_vec))  
+					if (r<r_cut) then
+						F = F + e*(48*s**12/r**14 - 6*s**6/r**8) * r_vec		
+						U(n) = U(n) + 5d-1*(4*e*(s/r)**12-(s/r)**6)
+					end if
 				end if		
 			end do 
 			end do 
-			end do		
-		end do
+			end do
+		end do			
 		vel(:,n) = vel(:,n) + F/m*dt	
+		v_2(n) = dot_product(vel(:,n),vel(:,n))
+		F_R(N_avSteps) = F_R(N_avSteps) + dot_product(pos(:,n), F)	 !ensemble average
 !		print *,"particle:", n, "/",  N_part, "velocity:", (VEL(i,n), i=1,3)
 	end do
+	
+	! Postion calculation
+	do n = 1,N_part 
+		pos(:,n) = pos(:,n) + vel(:,n)*dt			
+		do i = 1,3 !implements periodic boundary conditions
+			if (pos(i,n) < 0d0) then 
+				pos(i,n) = pos(i,n) + L_side
+			else if (pos(i,n) > L_side) then
+				pos(i,n) = pos(i,n) - L_side 
+			end if
+		end do		
+!		print *, "particle:", n, "/",  N_part, "position:", pos(:,n)
+	end do	
+	
+	Ekin = sum(m/2*V_2)
+	H = sum(U) + Ekin
+	Temp = 2*Kb/3*Ekin/N_part
+	P = N_part/(L_side**3)*Kb*T*(1 + 1/(3*Kb*T*N_part)* sum(F_R)/N_avSteps)
+	
+	print *, step, "t = ", time, "H =", H,  "T =", Temp, "P =", P,  "vel1 =", vel(1,1), "pos1 =", pos(1,1)	
+		
 end do	
 
 call plot_end
