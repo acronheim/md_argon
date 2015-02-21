4291437c85c2195b09d424b4104bb0fed4ae4519
Alexander Cronheim <alexander.cronheim@gmail.com>
2015-02-21 05:02:31 +0100

    update settings

--- argon_box.f90
+++ argon_box.f90
@@ -17,8 +17,8 @@ program argon_box
 	use md_plot
 	implicit none
 	
-	integer, parameter :: N_cell_dim = 4, velocity_rescale_steps = 100
-	real(8), parameter :: dt = 0.004_8, T_initial = 1d0, rho = 0.70_8, t_stop = 1d1
+	integer, parameter :: N_cell_dim = 4, velocity_rescale_steps = 50
+	real(8), parameter :: dt = 0.004_8, T_initial = 1d0, rho = 0.88_8, t_stop = 1d0
 	
 	integer, parameter :: N_cell = N_cell_dim**3, N_part = N_cell*4
 	real(8), parameter :: L_side = (N_part/rho)**(1._8/3), m = 1d0
