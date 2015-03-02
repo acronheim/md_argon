b758cf79dbe5a198f142a040b0c2f921cd55fca7
Alexander Cronheim <alexander.cronheim@gmail.com>
2015-03-02 14:28:30 +0100

    data-analysis: data blocking implemented

--- data_analysis.m
+++ data_analysis.m
@@ -1,5 +1,4 @@
-N_equilibriation = 200;
-
+%% data files
 filenames = {
 'energy_matrix-rho0.70-T0.9.dat'
 'energy_matrix-rho0.70-T1.0.dat'
@@ -23,8 +22,21 @@ filenames = {
 'energy_matrix-rho0.88-T1.1.dat'
 };
 
-N = length(filenames)
+%% parameters
+N = length(filenames);
+N_part = 864;
+N_equilibriation = 200;
+N_timesteps = 2501;
+N_measurement_length = N_timesteps - N_equilibriation;
+measurement_interval = N_equilibriation:1:N_timesteps;
+datablock_size = 100;
+N_blocks = (N_measurement_length - N_equilibriation) / datablock_size;
+Kb = 1;
+
+
 for i = 1:N
+    
+    %% load data
     density(i) = wstr2num(filenames{i}(18:21));
     T_init(i)  = wstr2num(filenames{i}(24:26));
     M = load(filenames{i});
@@ -33,4 +45,57 @@ for i = 1:N
     time(:,i)  = M(:,2);
     step(:,i)  = M(:,1);
     virial(:,i)= M(:,5);
+    
+%% measurements results
+    kinenergy(i) = mean(E_kin(measurement_interval, i));
+    potenergy(i) = mean(E_pot(measurement_interval, i));
+    totenergy(i) = kinenergy(i) + potenergy(i);
+    temperature(i) = 2*N_part/(3*(N_part-1)*Kb)*kinenergy(i);
+    compressure(i) = 1 + 1/(3*Kb*Temperature(i)*N_part)* mean(virial(measurement_interval, i));
+    
+    %% data blocking
+    for j = 1:N_blocks
+        E_kin_block(j) = mean( E_kin((N_equilibriation+1+(j-1)*datablock_size):1:(N_equilibriation+1+j*datablock_size),i));
+        E_pot_block(j) = mean( E_pot((N_equilibriation+1+(j-1)*datablock_size):1:(N_equilibriation+1+j*datablock_size),i));
+        E_kin_squared_block(j) = mean( (E_kin((N_equilibriation+1+(j-1)*datablock_size):1:(N_equilibriation+1+j*datablock_size),i)).^2);
+    end 
+    E_tot_block = E_pot_block + E_kin_block;
+    error_E_kin(i) = std(E_kin_block)/sqrt(N_blocks);
+    error_E_pot(i) = std(E_pot_block)/sqrt(N_blocks);
+    error_E_tot(i) = std(E_tot_block)/sqrt(N_blocks);
+    error_E_kin_squared(i) = std(E_kin_squared_block)/sqrt(N_blocks);
+    
+    %% specific heat
+    specificheat(i) = -1/(error_E_kin_squared(i)/(kinenergy(i))^2 - 2/(3*N_part));
+    
 end
+
+
+%% plotting
+
+figure % constant density
+selected_indices = density == 0.88
+plot(temperature(selected_indices), compressure(selected_indices))
+selected_indices = density == 0.80
+plot(temperature(selected_indices), compressure(selected_indices))
+selected_indices = density == 0.70
+plot(temperature(selected_indices), compressure(selected_indices))
+title('P(T)')
+xlabel('Temperature')
+ylabel('Compressibility factor')
+
+figure % constant temperature
+selected_indices = T_init == 1
+plot(density(selected_indices), compressure(selected_indices))
+title('P(rho), T =~ 1.0')
+xlabel('density')
+ylabel('Compressibility factor')
+
+figure %specific heat
+selected_indices = density == 0.80
+plot(temperature(selected_indices), specificheat(selected_indices))
+title('specificheat, rho =~ 0.88')
+xlabel('Temperature')
+ylabel('Compressibility factor')
+
+
