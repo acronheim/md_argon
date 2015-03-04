85e05456fbaed1189703a33d94c632a56d99a081
Alexander Cronheim <alexander.cronheim@gmail.com>
2015-03-04 14:04:56 +0100

    some improvements to the data analysis script

--- data_analysis.m
+++ data_analysis.m
@@ -1,5 +1,12 @@
+clear all
+close all
+
 %% data files
 filenames = {
+'energy_matrix-rho0.30-T3.0.dat'
+'energy_matrix-rho0.70-T0.3.dat'
+'energy_matrix-rho0.70-T0.5.dat'
+'energy_matrix-rho0.70-T0.7.dat'
 'energy_matrix-rho0.70-T0.9.dat'
 'energy_matrix-rho0.70-T1.0.dat'
 'energy_matrix-rho0.70-T1.1.dat'
@@ -20,17 +27,17 @@ filenames = {
 'energy_matrix-rho0.88-T0.9.dat'
 'energy_matrix-rho0.88-T1.0.dat'
 'energy_matrix-rho0.88-T1.1.dat'
+'energy_matrix-rho1.20-T0.5.dat'
 };
 
 %% parameters
 N = length(filenames);
 N_part = 864;
 N_equilibriation = 200;
-N_timesteps = 2501;
-N_measurement_length = N_timesteps - N_equilibriation;
-measurement_interval = N_equilibriation:1:N_timesteps;
+
+
 datablock_size = 100;
-N_blocks = (N_measurement_length - N_equilibriation) / datablock_size;
+
 Kb = 1;
 
 
@@ -46,21 +53,27 @@ for i = 1:N
     step(:,i)  = M(:,1);
     virial(:,i)= M(:,5);
     
+    %% parameters
+    N_timesteps = length(step(:,i));
+    N_measurement_length = N_timesteps - N_equilibriation;
+    measurement_interval = N_equilibriation:1:N_timesteps;
+    N_blocks = floor((N_measurement_length - N_equilibriation) / datablock_size);
+    
 %% measurements results
     kinenergy(i) = mean(E_kin(measurement_interval, i));
     potenergy(i) = mean(E_pot(measurement_interval, i));
     totenergy(i) = kinenergy(i) + potenergy(i);
     temperature(i) = 2*N_part/(3*(N_part-1)*Kb)*kinenergy(i);
-    compressure(i) = 1 + 1/(3*Kb*Temperature(i)*N_part)* mean(virial(measurement_interval, i));
+    compressure(i) = 1 + 1/(3*Kb*temperature(i)*N_part)* mean(virial(measurement_interval, i));
     
 %% time correlation length
-    input = E_pot(:,i)
-    timecorrelation = xcorr(input, zero_timecorrelation -1);
+    input = E_pot(measurement_interval,i);
+    timecorrelation = xcorr(input - mean(input));
     zero_timecorrelation = length(input);
     tau_pot(i) = 1/2*sum(timecorrelation/timecorrelation(zero_timecorrelation));
     
-    input = E_kin(:,i)
-    timecorrelation = xcorr(input, zero_timecorrelation -1, 'coeff');
+    input = E_kin(measurement_interval,i);
+    timecorrelation = xcorr(input - mean(input));
     zero_timecorrelation = length(input);
     tau_kin(i) = 1/2*sum(timecorrelation/timecorrelation(zero_timecorrelation));
     
@@ -71,9 +84,9 @@ for i = 1:N
         %E_kin_squared_block(j) = mean( (E_kin((N_equilibriation+1+(j-1)*datablock_size):1:(N_equilibriation+1+j*datablock_size),i)).^2);
     end 
     E_tot_block = E_pot_block + E_kin_block;
-    error_E_kin(i) = std(E_kin_block)/sqrt(N_blocks);
-    error_E_pot(i) = std(E_pot_block)/sqrt(N_blocks);
-    error_E_tot(i) = std(E_tot_block)/sqrt(N_blocks);
+    error_E_kin(i) = std(E_kin_block);
+    error_E_pot(i) = std(E_pot_block);
+    error_E_tot(i) = std(E_tot_block);
     %error_E_kin_squared(i) = std(E_kin_squared_block)/sqrt(N_blocks);
     
     %% specific heat
@@ -82,10 +95,14 @@ for i = 1:N
     
 end
 
-    error_E_kin
-    error_E_pot
-    error_E_tot
 %% plotting
+
+figure
+hold on
+plot(error_E_kin)
+plot(error_E_pot)
+plot(error_E_tot)
+title('error estimation energy')
 
 figure % constant density
 hold on
@@ -115,13 +132,26 @@ ylabel('Compressibility factor')
 
 
 %%
+
+for i = 1:15
 figure %energy fluctuations
 hold on
-i = 16
 plot(step(:,i), E_kin(:,i), '-r')
-plot(step(:,i), E_pot(:,i), '-b')
 plot(step(:,i), E_pot(:,i) + E_kin(:,i), '-k')
-title('equilibriation')
-xlabel('timestep')
-ylabel('Energy')
+plot(step(:,i), E_pot(:,i), '-b')
 
+title(['Energy per particle, \rho = ', num2str(density(i)), ', T = ', num2str(T_init(i))], 'fontsize', 18)
+xlabel('Timestep', 'fontsize', 16)
+ylabel('Energy (\epsilon)', 'fontsize', 16)
+legend('Kinetic energy', 'Total energy', 'Potential energy')
+set(gca,'fontsize',14)
+axis([0, 2501, -8, 4])
+end
+
+
+%%
+figure
+hold on 
+plot(tau_kin)
+plot(tau_pot)
+title('correlation time')
